0d14b77890901940085b1046b42d5288b45d2ff1
Gabriel Prevosto <gprevosto@oet.fr>
2022-03-16 10:01:10 +0100

    [GUI] Added the ability to select the target dimensions

--- ihm.py
+++ ihm.py
@@ -25,7 +25,7 @@ class MainWindow(wx.Frame):
     def initUI(self):
         """Builds and displays all the UI elements of the window"""
         panel = wx.Panel(self)
-        sizer = wx.GridBagSizer(3, 5)
+        sizer = wx.GridBagSizer(3, 6)
 
         self.panel = panel
         self.sizer = sizer
@@ -40,15 +40,32 @@ class MainWindow(wx.Frame):
         self.colorSizer = wx.FlexGridSizer(cols=4, vgap=2, hgap=5)
         sizer.Add(self.colorSizer, pos=(1, 0), span=(1,2), flag=wx.EXPAND)
         
+        self.dimensionSizer = wx.GridBagSizer(3,3)
+        dimensionMainHeader = wx.StaticText(panel, label="Dimensions :")
+        dimensionXHeader = wx.StaticText(panel, label="Length (mm) ")
+        dimensionYHeader = wx.StaticText(panel, label="Width (mm) ")
+        dimensionZHeader = wx.StaticText(panel, label="Height (mm) ")
+        self.dimensionXselect = wx.SpinCtrl(self.panel, min=10, max=1000, initial=100)
+        self.dimensionYselect = wx.SpinCtrl(self.panel, min=10, max=1000, initial=100)
+        self.dimensionZselect = wx.SpinCtrl(self.panel, min=1, max=100, initial=10)
+        self.dimensionSizer.Add(dimensionMainHeader, pos=(0, 0), span=(1,3))
+        self.dimensionSizer.Add(dimensionXHeader, pos=(1, 0))
+        self.dimensionSizer.Add(dimensionYHeader, pos=(1, 1))
+        self.dimensionSizer.Add(dimensionZHeader, pos=(1, 2))
+        self.dimensionSizer.Add(self.dimensionXselect, pos=(2, 0))
+        self.dimensionSizer.Add(self.dimensionYselect, pos=(2, 1))
+        self.dimensionSizer.Add(self.dimensionZselect, pos=(2, 2))
+        sizer.Add(self.dimensionSizer, pos=(2, 0), span=(1,2))
+        
         self.buttonGenerate = wx.Button(panel, label="Generate", size=(90, 28))
         self.buttonGenerate.Bind(wx.EVT_BUTTON,self.onGenerate)
-        sizer.Add(self.buttonGenerate, pos=(2, 0), span=(1,2), flag=wx.EXPAND)
+        sizer.Add(self.buttonGenerate, pos=(3, 0), span=(1,2), flag=wx.EXPAND)
         
         self.gaugeText = wx.StaticText(panel, label="Idle")
-        sizer.Add(self.gaugeText, pos=(3, 0))
+        sizer.Add(self.gaugeText, pos=(4, 0))
         
         self.gauge = wx.Gauge(panel, range=100)
-        sizer.Add(self.gauge, pos=(4,0), span=(1,2), flag=wx.EXPAND)
+        sizer.Add(self.gauge, pos=(5,0), span=(1,2), flag=wx.EXPAND)
         
         self.PhotoMaxSize = 100
         self.imageCtrl = wx.StaticBitmap(self.panel, wx.ID_ANY, 
@@ -227,7 +244,8 @@ class MainWindow(wx.Frame):
             MainWindow.callUpdateProgress(0, "Generating height map")
             colors = [ColorDefinition(color, self.getColorType(color), self.getParameter(color)) for color in self.colors]
             grayscaleImagePath, grayscaleImageReso = generateGreyScaleImage(self.imagePath, colors, self.pixel_list_labels, self.relevant_label_to_color_hexes)
-            meshMandatoryParams = MeshMandatoryParameters(self.imagePath, grayscaleImageReso)
+            desiredSize = (self.dimensionXselect.GetValue(), self.dimensionYselect.GetValue(),self.dimensionZselect.GetValue())
+            meshMandatoryParams = MeshMandatoryParameters(self.imagePath, grayscaleImageReso, desiredSize=desiredSize)
             MainWindow.callUpdateProgress(50, "Generating STL file")
             generateSTL(grayscaleImagePath, meshMandatoryParams)
             MainWindow.callUpdateProgress(100)
